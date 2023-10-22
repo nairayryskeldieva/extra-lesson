ceda8d41e83c158609ee810bbd9caf6dcc805f5e
nairayryskeldieva <148675872+nairayryskeldieva@users.noreply.github.com>
2023-10-22 15:47:54 +0600

    added files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Background } from "./components/Background";
+
+export const App = () => {
+  return (
+    <div>
+      <span>Its App!</span>
+      <Background />
+    </div>
+  );
+};
---
+++ src/components/Background.jsx
@@ -0,0 +1,9 @@
+import styles from "./Background.module.css";
+
+export const Background = () => {
+  return (
+    <div className={styles.container}>
+      <h2>Background</h2>
+    </div>
+  );
+};
